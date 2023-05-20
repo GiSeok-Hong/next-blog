f47e22a0c42d8a356eac7384dec5790ea2469756
Giseok Hong <hgsok3056@gmail.com>
2023-05-20 14:47:46 +0900

    feat: FeaturedPost 관련 작업 작성

---
+++ src/components/PostsGrid.tsx
@@ -0,0 +1,14 @@
+import { Post } from "@/service/posts";
+import PostCard from "./PostCard";
+
+export default function PostsGrid({ posts }: { posts: Post[] }) {
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.path}>
+          <PostCard post={post} />
+        </li>
+      ))}
+    </ul>
+  );
+}
