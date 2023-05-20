486f388dfed0eb1b382aa33c3bba0c7ece16b44a
Giseok Hong <hgsok3056@gmail.com>
2023-05-20 15:14:16 +0900

    design: FeaturedPost 디자인 작성

--- src/components/FeaturedPosts.tsx
+++ src/components/FeaturedPosts.tsx
@@ -4,8 +4,8 @@ import PostsGrid from "./PostsGrid";
 export default async function FeaturedPosts() {
   const posts = await getFeaturedPosts();
   return (
-    <section>
-      <h2 className="text-2xl font-bold">Featured Posts</h2>
+    <section className="my-4">
+      <h2 className="text-2xl font-bold my-2">Featured Posts</h2>
       <PostsGrid posts={posts} />
     </section>
   );
--- src/components/PostsGrid.tsx
+++ src/components/PostsGrid.tsx
@@ -3,7 +3,7 @@ import PostCard from "./PostCard";
 
 export default function PostsGrid({ posts }: { posts: Post[] }) {
   return (
-    <ul>
+    <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {posts.map((post) => (
         <li key={post.path}>
           <PostCard post={post} />
