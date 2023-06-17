42b850a1b2c6862848b41a4d25a9588b7bdf03f7
Giseok Hong <hgsok3056@gmail.com>
2023-06-17 16:20:16 +0900

    feat: contact page 기본틀 작성

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,41 @@
+import ContactForm from "@/components/ContactForm";
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
+
+const LINKS = [
+  {
+    icon: <AiFillGithub />,
+    url: "https://github.com/GiSeok-Hong",
+  },
+  {
+    icon: <AiFillLinkedin />,
+    url: "",
+  },
+  {
+    icon: <AiFillYoutube />,
+    url: "",
+  },
+];
+
 export default function ContactPage() {
   return (
-    <>
-      <h1>contact page</h1>
-    </>
+    <section className="flex flex-col items-center">
+      <h2 className="text-3xl font-bold my-2">Contact Me</h2>
+      <p>hgsok3056@naver.com</p>
+      <ul className="flex gap-4 my-2">
+        {LINKS.map((link, index) => (
+          <a
+            key={index}
+            href={link.url}
+            target="_blank"
+            rel="noreferrer"
+            className="text-5xl hover:text-yellow-400"
+          >
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className="text-3xl font-bold my-8">Or send me an email</h2>
+      <ContactForm />
+    </section>
   );
 }
